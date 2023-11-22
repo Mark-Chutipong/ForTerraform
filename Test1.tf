4aa42e0b29140d4ffe7ec15f3261da91c1d864cf
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-22 10:24:58 +0700

    RoleChange2G

--- Test1.tf
+++ Test1.tf
@@ -42,7 +42,7 @@ resource "aws_iam_role" "test_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = "aws_iam_role.test_role.name"
+  role       = "${aws_iam_role.test_role.name}"
   policy_arn = "${aws_iam_policy.test_policy.arn}"
 }
 
