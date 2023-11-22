c9ec53ee12e2bff0326caf349a75666bcacf9ad9
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-22 10:34:42 +0700

    KMSKeyandPolicy

--- Test1.tf
+++ Test1.tf
@@ -81,3 +81,26 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+resource "aws_kms_key" "example" {
+  description = "example"
+}
+
+resource "aws_kms_key_policy" "example" {
+  key_id = aws_kms_key.example.id
+  policy = jsonencode({
+    Id = "example"
+    Statement = [
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
