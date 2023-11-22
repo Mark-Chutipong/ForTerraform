e99d82e3de5539550b0218a611814c62cadd6c8d
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-22 21:11:05 +0700

    AddKey3

--- Test1.tf
+++ Test1.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   security_groups = [aws_security_group.allow_tls.name]
-  key_name = "deployer"
+  key_name = aws_key_pair.deployer.key_name
  
   tags = {
     Name = "Test"
