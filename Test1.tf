c1b88b16df835549d45ce32eb92eb31278fcb1e5
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-23 11:31:34 +0700

    Docker10

--- Test1.tf
+++ Test1.tf
@@ -137,10 +137,10 @@ resource "aws_instance" "example" {
   security_groups = [aws_security_group.allow_tls.name]
   key_name = aws_key_pair.deployer.key_name
   user_data = <<-EOF
-    "#!/bin/bash
+    #!/bin/bash
     sudo amazon-linux-extras install docker -y
     sudo service docker start
-    sudo usermod -a -G docker ec2-user"
+    sudo usermod -a -G docker ec2-user
   EOF
   
 
