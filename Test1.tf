953b42d3189d969d6c7d7bbecc75209d30e70eea
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-23 15:03:15 +0700

    Docker16

--- Test1.tf
+++ Test1.tf
@@ -142,6 +142,7 @@ resource "aws_instance" "example" {
     sudo yum install -y docker
     sudo service docker start
     sudo usermod -a -G docker ec2-user
+    sudo docker run -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home jenkins:2.60.3
     EOF
 
   #root_block_device {
