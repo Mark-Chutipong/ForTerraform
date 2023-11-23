5c7177eb2227066d5911df5c2924b1c9c3130136
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-23 11:04:23 +0700

    Docker5

--- Test1.tf
+++ Test1.tf
@@ -137,9 +137,9 @@ resource "aws_instance" "example" {
   security_groups = [aws_security_group.allow_tls.name]
   key_name = aws_key_pair.deployer.key_name
   user_data_base64 = <<EOF
-  #!/bin/bash
+  "#!/bin/bash
   sudo yum install -y docker
-  sudo usermod -a -G docker ec2-user
+  sudo usermod -a -G docker ec2-user"
   EOF
   
 
