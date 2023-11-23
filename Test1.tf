76c2f7989a5ffb09375482ca5fd3bf57fa57faba
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-23 14:08:37 +0700

    Docker14

--- Test1.tf
+++ Test1.tf
@@ -136,7 +136,7 @@ resource "aws_instance" "example" {
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   security_groups = [aws_security_group.allow_tls.name]
   key_name = aws_key_pair.deployer.key_name
-  user_data = <<-EOF
+  user_data = <<EOF
     #!/bin/bash
     sudo yum update -y
     sudo yum install -y docker
