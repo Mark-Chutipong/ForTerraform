c1beaabd36f85876f2c7e4c269d97ea06abab148
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-28 12:26:45 +0700

    Test7

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   ami                  = var.ami
   instance_type        = var.instance_type
   iam_instance_profile = "ec2_profile"
-  security_groups      = "allow_tls"
+  security_groups      = ["allow_tls"]
   key_name             = "deployer-key"
   user_data            = file("ec2_instance/userdata.sh")
 
