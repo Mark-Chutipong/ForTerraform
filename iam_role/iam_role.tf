82fb4f5853335984cd6cc53f97c0bae2e8ac19f4
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-28 10:34:23 +0700

    Test2

--- iam_role/iam_role.tf
+++ iam_role/iam_role.tf
@@ -1,7 +1,24 @@
 provider "aws" {
   region = var.region
 }
+resource "aws_iam_policy" "test_policy" {
+  name = var.name
 
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = var.version
+    Statement = [
+      {
+        Action = [
+          var.action,
+        ]
+        Effect   = var.effect
+        Resource = 0
+      },
+    ]
+  })
+}
 resource "aws_iam_role" "test_role" {
   name = var.name
 
@@ -21,5 +38,14 @@ resource "aws_iam_role" "test_role" {
   tags = {
     Name = [var.tags[0]]
   }
+}
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = aws_iam_policy.test_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = [aws_iam_role.test_role.name]
 }
 
