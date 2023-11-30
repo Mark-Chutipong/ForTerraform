02c8d790df9401e714f8dbc376359d27dccc8129
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-30 16:45:40 +0700

    T27

--- modules/kms/kms.tf
+++ modules/kms/kms.tf
@@ -12,20 +12,17 @@ resource "aws_kms_alias" "a" {
 }
 
 # KMS Key Policy
-data "aws_kms_key_policy_document" "document" {
+data "aws_iam_policy_document" "document" {
   statement {
-    principals {
-      AWS = "kms:*"
-    }
+    
     effect = "Allow"
-    action = "kms:*"
-    Resource = "*"
-    Sid      = "Enable IAM User Permissions"
+    
+    
   }
 }
 resource "aws_kms_key_policy" "default" {
   key_id = aws_kms_key.default.key_id
-  policy = data.aws_kms_key_policy_document.document.json
+  policy = data.aws_iam_policy_document.document.json
 }
 
 
