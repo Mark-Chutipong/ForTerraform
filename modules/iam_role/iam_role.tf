8b47f1965f8d4fed6c698bd40c6edde86798387a
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-30 14:43:56 +0700

    T13

--- modules/iam_role/iam_role.tf
+++ modules/iam_role/iam_role.tf
@@ -33,6 +33,6 @@ resource "aws_iam_role_policy_attachment" "default" {
 }
 
 resource "aws_iam_instance_profile" "default" {
-  name = aws_iam_instance_profile.default.name
+  name = var.iam_instance_profile_name
   role = aws_iam_role.default.name
 }
