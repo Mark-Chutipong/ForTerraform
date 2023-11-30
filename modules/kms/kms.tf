e41f524a0e38788f4457322fd02bc09f00c017e6
“Mark-Chutipong” <markchutipong@gmail.com>
2023-11-30 16:51:07 +0700

    T28

--- modules/kms/kms.tf
+++ modules/kms/kms.tf
@@ -16,7 +16,10 @@ data "aws_iam_policy_document" "document" {
   statement {
     
     effect = "Allow"
-    
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
     
   }
 }
