8c98719d522b7e6b6f8f7e79d21a0e96e3810493
markchutipong <markchutipong@gmail.com>
2024-01-14 09:14:10 +0700

    add config docker

--- configs/prereq.sh
+++ configs/prereq.sh
@@ -1,4 +1,6 @@
 #/bash
 
-useradd jeknins -u 1001 -g 1000 -m -s /bin/bash
-mkdir -p /var/jenkins_home
+sudo useradd jenkins -u 1001 -g 1000 -m -s /bin/bash
+sudo mkdir -p /var/jenkins_home
+sudo chown jenkins:ec2-user /var/jenkins_home
+sudo apt-get -y install docker-ce
