0ebe3d0335eb5f30b6d9dbd4fa9e52a9e35089eb
“Mark-Chutipong” <markchutipong@gmail.com>
2024-01-21 16:03:14 +0700

    Revert

--- backend.tf
+++ backend.tf
@@ -1,12 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "s3-tfstate-backend"
-    key    = "ec2/test"
-    region = "ap-southeast-1"
-  }
-  backend "s3" {
-    bucket = "s3-tfstate-backend2"
-    key    = "ec2/test2"
+    key    = "ec2"
     region = "ap-southeast-1"
   }
 }
