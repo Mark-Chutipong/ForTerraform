0bd00656dd6d933b6a58a5e65aca45abeca681d6
“Mark-Chutipong” <markchutipong@gmail.com>
2024-01-21 16:01:32 +0700

    Backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,12 @@
 terraform {
   backend "s3" {
     bucket = "s3-tfstate-backend"
-    key    = "ec2"
+    key    = "ec2/test"
+    region = "ap-southeast-1"
+  }
+  backend "s3" {
+    bucket = "s3-tfstate-backend2"
+    key    = "ec2/test2"
     region = "ap-southeast-1"
   }
 }
