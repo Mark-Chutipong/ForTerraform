a05a3d8b6d63989954f190f993ed4170e3cbbabb
“Mark-Chutipong” <markchutipong@gmail.com>
2024-01-26 15:07:27 +0700

    key pair

--- variables.tf
+++ variables.tf
@@ -35,9 +35,9 @@ variable "aws_ec2_key_name" {
   default = ""
 }
 
-variable "aws_ec2_create_keypair" {
+variable "aws_ec2_public_key" {
   type    = string
-  default = ""
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0Fg8D/pbeFXV5e5kDuqyfkApq2V+8mT28HvGHTIu6YYAG6UZc3QXFrrxtNVr1aEv8CQi3Gx/4Aq0hp2dmUR5wDMYL7hCeqf1XAyTb8d6p9KoKXazv6RgmgaugvzPSjDH+1wdKVEA0upv05o5WNdtizVnkBxGgCN9y5dWqV0UewBjZeM+N3E9ZiOjP5djGqGFEbGLuI62JseDlxmjJywoohsjoWJVB0XdK+stA4NNvWWHv2MR0qHmwx7nxUPu8WojKEcsVhQvII/LY2tFWO1kSaTTYJ6i8Bj5PRYTVBenOOkTosVWh2zN8MbIWuA3KsBvOZwHIWxhMa5Ti6Mweffp9"
 }
 
 
