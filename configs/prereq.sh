1245f4261611b0641353c8285599f8440e568aca
“Mark-Chutipong” <markchutipong@gmail.com>
2024-02-09 00:05:21 +0700

    prereq.sh change

--- configs/prereq.sh
+++ configs/prereq.sh
@@ -3,4 +3,4 @@
 sudo useradd jenkins -u 1001 -g 1000 -m -s /bin/bash
 sudo mkdir -p /var/jenkins_home
 sudo chown jenkins:ec2-user /var/jenkins_home
-sudo apt-get -y install docker-ce
+sudo yum -y install docker
